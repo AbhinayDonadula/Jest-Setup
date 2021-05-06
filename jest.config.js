e162608efdf9fd263557cbb3b7e56e63e7cd818c
Abhinay Donadula <AbhinayDonadula@users.noreply.github.com>
2021-05-06 14:48:57 -0500

    multiple jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    displayName: 'Abhinay',
     testEnvironment: 'jest-environment-jsdom',
     moduleNameMapper: {
         '\\.css$': require.resolve('./src/testUtils/style-mock.js'),
@@ -7,4 +8,14 @@ module.exports = {
         'jest-extended',
         '@testing-library/jest-dom/extend-expect',
     ],
+    collectCoverageFrom: ['**/src/app/*.js'],
+    coverageThreshold: {
+        global: {
+            statements: 60,
+            branches: 100,
+            functions: 45,
+            lines: 100,
+        },
+    },
+    projects: ['<rootDir>/jest.config.js', '<rootDir>/jest.lint.js'],
 };
--- src/app/utils.js
+++ src/app/utils.js
@@ -2,7 +2,7 @@ export const add = (a, b) => a + b;
 export const sub = (a, b) => a - b;
 
 export const getAbove3GPAStuds = (students = []) => {
-    debugger;
+    const a = 20;
     const result = students.filter((each) => each.gpa > 3);
     return result;
 };
