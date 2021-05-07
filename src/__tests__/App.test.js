0311f138bb2b15f0339d80991df6ed28ef680fcd
Abhinay Donadula <AbhinayDonadula@users.noreply.github.com>
2021-05-07 07:42:21 -0500

    RTL updates

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { getQueriesForElement } from '@testing-library/dom';
 import App from '../app/App';
 
-test('renders app with title Abhinay', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App title="Jest is the best" />, div);
-    expect(div.querySelector('span').innerHTML).toEqualCaseInsensitive(
-        'Jest is the Best'
-    );
+describe('App test cases', () => {
+    const testWith = 'Jest is the best';
+
+    test('successfully renders - no RTL', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App title={testWith} />, div);
+
+        // using in built dom selectors
+        const span = div.querySelector('span');
+        const spanHTML = span.innerHTML;
+        expect(spanHTML).toEqualCaseInsensitive(testWith);
+
+        expect(span).toHaveTextContent(testWith);
+
+        // using queries from testing library
+        const { getByTitle } = getQueriesForElement(div);
+        const spanEl = getByTitle(testWith);
+        expect(spanEl).toHaveTextContent(testWith);
+    });
+
+    test('successfully renders with RTL', () => {
+        const { getByTitle, debug } = render(<App title="Jest is the best" />);
+        debug();
+
+        const spanEl = getByTitle(testWith);
+        expect(spanEl).toHaveTextContent(testWith);
+    });
+
+    test.only('user event - RTL', () => {
+        const { getByPlaceholderText, debug, getByRole } = render(
+            <App title="Jest is the best" />
+        );
+
+        const input = getByPlaceholderText('type here');
+        debug(input);
+        user.type(input, '20');
+        expect(getByRole('alert')).toHaveTextContent('hi');
+    });
 });
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,9 +2,22 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 function App({ title }) {
+    const [val, setVal] = React.useState('');
+    const handleChange = (e) => {
+        debugger;
+        console.log('##########', e);
+        setVal(e.target.value);
+    };
+
     return (
         <div>
-            <span>{title}</span>
+            <span title={title}>{title}</span>
+            <input
+                value={val}
+                onChange={handleChange}
+                placeholder="type here"
+            />
+            {val.length ? <span role="alert">hi</span> : null}
         </div>
     );
 }
